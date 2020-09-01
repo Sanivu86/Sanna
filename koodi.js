98a640a8cc04e552dafa3ba0bdaed93472d1f308
Sanivu86 <59826845+Sanivu86@users.noreply.github.com>
2020-09-01 21:53:26 +0300

    drtsyhdj

---
+++ koodi.js
@@ -0,0 +1,8 @@
+
+
+var otsikko = document.getElementById("otsikkoni");
+otsikko.style = "color:red; background-color: black;"
+
+otsikko.innerHTML = "Muutan otsikon tällä käskyllä!"
+
+document.write(otsikko.getAttribute("oma"));
