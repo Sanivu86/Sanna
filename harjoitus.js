51a0d51cfb733a9f13901339a945009157acc3e2
Sanivu86 <59826845+Sanivu86@users.noreply.github.com>
2020-12-14 01:11:15 +0200

    fg

---
+++ harjoitus.js
@@ -0,0 +1,9 @@
+//Varoitusteksti, joka näkyy ruudulla
+/* Tässä on tehty muuttuja, joka on funktio, jolle annetaan viesti, 
+joka toistetaan, kun funktio toteutetaan */
+
+var alertMessage = function(message)
+{ alert( message)
+}
+
+  alertMessage("Tämä on harjoitus!!!")
